db097e773340ef4dacae86c9b60f833009bd025f
Achintya <achintya26022006@gmail.com>
2026-06-27 23:27:07 +0530

    Added Ques

--- dp/1097.cpp
+++ dp/1097.cpp
@@ -21,7 +21,7 @@ ll funct(int i,int j,int n,vector<int>& arr,int player){
     if(player==1){
         takeFirst = funct(i+1,j,n,arr,1-player);
         takeLast = funct(i,j-1,n,arr,1-player);
-         return dp[i][j][player] = min(takeFirst,takeLast);
+        return dp[i][j][player] = min(takeFirst,takeLast);
     }else{
         takeFirst = arr[i] + funct(i+1,j,n,arr,1-player);
         takeLast = arr[j] + funct(i,j-1,n,arr,1-player);
@@ -36,41 +36,32 @@ int main() {
     while (t--) {
         int n;
         cin >> n;
+        ll total = 0;
         vector<int> arr(n);
         for (int i = 0; i < n; i++) {
             cin >> arr[i];
+            total+=arr[i];
         }
-        int i = 0;
-        int j = n-1;
         // dp.resize(n+1, vector<vector<ll>>(n+1, vector<ll>(2, -1)));
         // ll ans = funct(i,j,n,arr,0);
         // cout<<ans;
+        vector<vector<ll>> dp(n, vector<ll>(n));
 
-        dp.resize(n+1, vector<vector<ll>>(n+1, vector<ll>(2, 0)));
-        for(int i=0;i<n;i++){
-            dp[i][i][0] = arr[i];
-            dp[i][i][1] = 0;
-        }
+        for(int i = 0; i < n; i++)
+            dp[i][i] = arr[i];  
 
-        for(int len=2; len<=n; len++){
-
-            for(int i=0; i+len-1<n; i++){
-
+        for(int len = 2; len <= n; len++) {
+            for(int i = 0; i + len - 1 < n; i++) {
                 int j = i + len - 1;
 
-                dp[i][j][1] =
-                    min(
-                        dp[i+1][j][0],
-                        dp[i][j-1][0]
-                    );
-
-                dp[i][j][0] =
-                    max(
-                        arr[i] + dp[i+1][j][1],
-                        arr[j] + dp[i][j-1][1]
-                    );
+                dp[i][j] = max(
+                    arr[i] - dp[i+1][j],
+                    arr[j] - dp[i][j-1]
+                );
             }
-        }   
-        cout << dp[0][n-1][0];
+        }
+
+        ll diff = dp[0][n-1];
+        cout << (total + diff) / 2 << '\n';
     }
 }
